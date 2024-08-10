b6dd194fe06712598a26db25df7b8569ac40342e
yusto <yuusufmahud@gmail.com>
2024-08-10 13:14:15 +0300

    added orders Slice RTK

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,4 +1,5 @@
 export const BASE_URL = "";
 
 export const USERS_URL = "/api/users";
-export const PRODUCTS_URL = '/api/products'
+export const PRODUCTS_URL = "/api/products";
+export const ORDERS_URL = "/api/orders";
---
+++ src/redux/orderSlice.js
@@ -0,0 +1,16 @@
+import { apiSlice } from "./apiSlice";
+import { ORDERS_URL } from "./constants";
+
+const orderApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    createOrder: builder.mutation({
+      query: (order) => ({
+        url: `${ORDERS_URL}/create`,
+        method: "POST",
+        body: { ...order },
+      }),
+    }),
+  }),
+});
+
+export const { useCreateOrderMutation } = orderApiSlice;
